a2f22118d96172c36caa06706067f47fa2afad68
huyco940-eng <huyco940@gmail.com>
2026-01-19 17:00:27 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int main() {
+        MessageBoxA(NULL, 
+        "Chao mung anh trai den voi vipPanel!\nDeveloper: DevMinhKha\nStatus: ON", 
+        "vipPanel Premium", 
+        MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
