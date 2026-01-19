d2c5fdcbcf264e1af2e94f1241112ecc8c58a04c
huyco940-eng <huyco940@gmail.com>
2026-01-19 17:35:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,53 @@
 #include <windows.h>
-#include <vector>
 
-// Hàm ép mã máy (Dành cho pháp sư thực thụ)
-void PatchEx(LPVOID address, std::vector<unsigned char> bytes, HANDLE hProcess) {
-    DWORD oldProtect;
-    // Mở khóa bộ nhớ game để ghi đè
-    VirtualProtectEx(hProcess, address, bytes.size(), PAGE_EXECUTE_READWRITE, &oldProtect);
-    WriteProcessMemory(hProcess, address, bytes.data(), bytes.size(), NULL);
-    // Khóa lại như cũ để tránh bị phát hiện
-    VirtualProtectEx(hProcess, address, bytes.size(), oldProtect, &oldProtect);
+// ID cho các nút
+#define ID_AUTO_PULL 101
+
+bool b_AutoPull = false;
+
+// Hàm kéo tâm tự động (Macro)
+void DragShoot() {
+    if (b_AutoPull && (GetAsyncKeyState(VK_LBUTTON) & 0x8000)) {
+        // Tự động nhích chuột lên trên một chút để vào đầu
+        mouse_event(MOUSEEVENTF_MOVE, 0, -5, 0, 0); 
+        Sleep(20); // Tốc độ kéo
+    }
 }
 
-// Khi nhấn nút AIMBOT, anh gọi lệnh này:
-// Giả sử mã để ghim tâm là: 0x90, 0x90 (lệnh NOP trong assembly)
-void KichHoatAimbot(HANDLE hProc, DWORD offset) {
-    std::vector<unsigned char> aimbotCode = { 0x90, 0x90, 0x90 }; 
-    PatchEx((LPVOID)offset, aimbotCode, hProc);
+LRESULT CALLBACK WindowProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+    switch (msg) {
+    case WM_CREATE:
+        CreateWindowA("static", "--- FF vipPanel Macro ---", WS_VISIBLE | WS_CHILD | SS_CENTER, 10, 10, 260, 20, hWnd, NULL, NULL, NULL);
+        CreateWindowA("button", "BAT KEO TAM (HOLD L-CLICK)", WS_VISIBLE | WS_CHILD | BS_AUTOCHECKBOX, 20, 50, 240, 40, hWnd, (HMENU)ID_AUTO_PULL, NULL, NULL);
+        CreateWindowA("static", "Trang thai: Dang hoat dong", WS_VISIBLE | WS_CHILD | SS_CENTER, 10, 110, 260, 20, hWnd, NULL, NULL, NULL);
+        
+        // Tạo vòng lặp kiểm tra phím
+        SetTimer(hWnd, 1, 10, NULL);
+        break;
+    case WM_TIMER:
+        DragShoot();
+        break;
+    case WM_COMMAND:
+        if (LOWORD(wp) == ID_AUTO_PULL) {
+            b_AutoPull = !b_AutoPull;
+        }
+        break;
+    case WM_DESTROY: PostQuitMessage(0); break;
+    default: return DefWindowProc(hWnd, msg, wp, lp);
+    }
+    return 0;
+}
+
+int main() {
+    WNDCLASSW wc = {0};
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = L"vipPanelMacro";
+    wc.lpfnWndProc = WindowProcedure;
+    RegisterClassW(&wc);
+    HWND hWnd = CreateWindowW(L"vipPanelMacro", L"vipPanel FF - MinhKha", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 400, 200, 300, 180, NULL, NULL, NULL, NULL);
+    MSG msg = {0};
+    while (GetMessage(&msg, NULL, 0, 0)) { DispatchMessage(&msg); }
+    return 0;
 }
